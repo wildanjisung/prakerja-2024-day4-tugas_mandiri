4d40e9ccaf5dc1f1e9197b1fd49ac01385cb6dc2
wildanjisung <wildanjisung@gmail.com>
2024-03-03 17:02:55 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tugas Mandiri 4
+- lebar inputbox full
+- hapus, edit
+- inline edit
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  data() {
+    return {
+      item: '',
+      list: [],
+      isErrorInput: false
+    }
+  },
+  methods: {
+    simpan() {
+      // validasi
+      if(this.item == '') {
+        this.isErrorInput = true
+      } else {
+        // fungsi simpan ke array
+        this.list.push(this.item)
+        
+        // reset inputbox
+        this.item = '';
+
+        // remove error message
+        this.isErrorInput = false
+      }
+
+    }
+  },
+  computed: {
+    isMoreThanFour: function () {
+      return this.list.length >= 4;
+    }
+  },
+}
+
+</script>
+
+<template>
+  <!-- list -->
+  <ul>
+    <li v-for="(todo, index) in list" :key="index">{{ todo }}</li>
+  </ul>
+
+  <!-- input -->
+  <input type="text" v-model="item">
+  <button @click="simpan">Simpan</button>
+  <p v-if="isErrorInput" class="text-error">Inputan tidak boleh kosong</p>
+  <p v-if="isMoreThanFour">{{ 'Hebat' }}</p>
+</template>
+
+<style scoped>
+.text-error {
+  background-color: red;
+  color: white;
+}
+</style>
